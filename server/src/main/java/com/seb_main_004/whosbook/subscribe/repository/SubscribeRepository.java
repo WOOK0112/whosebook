2d911d8ce37b2d9287712dd9185adaa9a255b9b5
김동욱 <dong930112@gmail.com>
2024-02-13 18:07:23 +0900

    refactor: 구독조회기능 리팩터링

--- server/src/main/java/com/seb_main_004/whosbook/subscribe/repository/SubscribeRepository.java
+++ server/src/main/java/com/seb_main_004/whosbook/subscribe/repository/SubscribeRepository.java
@@ -2,6 +2,8 @@ package com.seb_main_004.whosbook.subscribe.repository;
 
 import com.seb_main_004.whosbook.member.entity.Member;
 import com.seb_main_004.whosbook.subscribe.entity.Subscribe;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,6 +13,6 @@ public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
 
     //구독한 멤버와 구독된 멤버 외래키를 통해 구독객체를 탐색
     Optional<Subscribe> findBySubscriberAndSubscribedMember(Member subscriber, Member subscribedMember);
-    List<Subscribe> findBySubscriber(Member subscriber);
+    Page<Subscribe> findBySubscriber(Member subscriber, Pageable pageable);
 
 }
